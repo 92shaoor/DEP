bb8dbf11cbace03e82f410e2c2ea9fbabc06e9d5
92shaoor <147979187+92shaoor@users.noreply.github.com>
2024-09-18 13:57:01 +0500

    Delete Task 02 directory

--- Task 02/models/post.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const postSchema = new mongoose.Schema({
-    title: String,
-    body: String,
-    createdAt: { type: Date, default: Date.now }
-});
-
-module.exports = mongoose.model('Post', postSchema);
